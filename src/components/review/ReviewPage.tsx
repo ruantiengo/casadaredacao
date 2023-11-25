915ebd9932ef2871dc3aabb1629455e70a2c5d86
Ruan Tiengo <ruantiengo@gmail.com>
2023-11-25 14:21:20 -0300

    save review page

--- src/components/review/ReviewPage.tsx
+++ src/components/review/ReviewPage.tsx
@@ -4,7 +4,7 @@ import Slider from './slider'
 const ReviewPage = () => {
   return (
     <div className='min-h-screen flex flex-col items-center pt-24'>
-        <h1 className='text-[48px] font-bold text-center'>O que os clientes dizem</h1>
+        <h1 className='text-[48px] font-bold text-center  '>O que os clientes dizem</h1>
         <Slider/>
     </div>
   )
