00cf54915755edc1c1cdee6680d84579bda2102d
Ruan Tiengo <ruan.rocha@contractor.rhimagnesita.com>
2024-01-11 19:16:35 -0300

    add button wpp

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,24 +1,25 @@
 import Image from "next/image";
 import React from "react";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { CardWithForm } from "./card-whatsapp";
 import { Button } from "../ui/button";
+import Link from "next/link";
 
 const Hero = () => {
   return (
     <div className="container transition-all duration-150 h-screen flex items-center justify-center">
       <div className="flex w-full items-center justify-center relative flex-col xl:justify-evenly xl:flex-row gap-8 xl:gap-0">
         <div className="flex flex-col items-start justify-start text-start">
-            <h1 className="text-[42px] lg:text-[64px] font-bold lg:w-[540px] pb-8">Venha aprender a fazer uma boa redação.</h1>
-            <span className="text-md lg:text-xl lg:w-[400px]">Curso on-line, de alto desempenho, com aulas semanais ao vivo, material exclusivo e atualizado, plantões, correções e muito mais!</span>
-            <Button className="h-[50px] text-lg font-[500] mt-20">Entre em contato</Button>
+          <h1 className="text-[42px] lg:text-[48px] 2xl:text-[64px] font-bold lg:w-[540px] pb-8">
+            Venha aprender a fazer uma boa redação.
+          </h1>
+          <span className="text-md lg:text-xl lg:w-[400px]">
+            Curso on-line, de alto desempenho, com aulas semanais ao vivo,
+            material exclusivo e atualizado, plantões, correções e muito mais!
+          </span>
+          <Link href="https://wa.me/553199381037" target="_blank">
+            <Button className="h-[50px] text-lg font-[500] mt-20">
+              Entre em contato
+            </Button>
+          </Link>
         </div>
         <div className="hidden xl:block xl:w-[550px] xl:h-[550px]">
           <div className="w-[300px] h-[300px] xl:w-[550px] xl:h-[550px] linearPurple rounded-full animate-spin transition-all absolute"></div>
@@ -30,9 +31,7 @@ const Hero = () => {
             className="absolute flex items-center justify-center"
           />
         </div>
-
       </div>
-
     </div>
   );
 };
